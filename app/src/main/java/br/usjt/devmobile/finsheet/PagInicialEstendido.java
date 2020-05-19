eaea9906a4a7f3bef509fe634ff5a4a0087411d9
Vinicius G <vinicius-leal1999@outlook.com>
2020-05-18 23:41:02 -0300

    Primeira versao

---
+++ app/src/main/java/br/usjt/devmobile/finsheet/PagInicialEstendido.java
@@ -0,0 +1,14 @@
+package br.usjt.devmobile.finsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PagInicialEstendido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pag_inicial_estendido);
+    }
+}
---
+++ app/src/main/java/br/usjt/devmobile/finsheet/PagInicialNega.java
@@ -0,0 +1,14 @@
+package br.usjt.devmobile.finsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PagInicialNega extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pag_inicial_nega);
+    }
+}
